253ae9e4013c549cf33682e3fc20be6344549f14
kdervic1 <kdervic1@etf.unsa.ba>
2021-05-01 14:59:27 +0200

    initial commit

---
+++ src/main/java/com/project/zulexbackend/ZulexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.zulexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZulexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZulexBackendApplication.class, args);
+	}
+
+}
